954e1af2752d002edb9f256a843bd55fbe685ecc
z4tz <z4tz87@gmail.com>
2025-12-01 22:18:06 +0100

    Reworked day 1 slightly

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -7,15 +7,18 @@ fn part1_and_part2(input: &str) -> String {
     let mut pointer = 50;
     let mut counter1 = 0;
     let mut counter2 = 0;
-    for number in data {
-        if number > 0 {
-            counter2 += (pointer + number) / 100;
+    for (turn, number) in data {
+        match turn {
+            Turn::Right => {
+                counter2 += (pointer + number) / 100;
+                pointer = (number + pointer ).rem_euclid(100);
+            }
+            Turn::Left => {
+                let rev_pointer = (100 - pointer) % 100;  // reverse the dial and treat it like a right turn
+                counter2 += (rev_pointer + number) / 100;
+                pointer = (pointer - number ).rem_euclid(100);
+            }
         }
-        else {
-            let rev_pointer = (100 - pointer) % 100;  // reverse the dial and treat it like a normal turn
-            counter2 += (rev_pointer + number.abs()) / 100;
-        }
-        pointer = (number + pointer ).rem_euclid(100);
         if pointer == 0 {
             counter1 += 1;
         }
@@ -23,8 +26,15 @@ fn part1_and_part2(input: &str) -> String {
     format!("{counter1}, {counter2}")
 }
 
-fn parse_input(input: &str) -> Vec<i32> {
-    input.replace('L', "-").replace('R', "")
-        .lines().map(|l| l.parse().unwrap())
-        .collect()
+fn parse_input(input: &str) -> Vec<(Turn, i32)> {
+    input.lines()
+        .map(|line| {
+            let turn = if line.starts_with("L") {Turn::Left} else {Turn::Right};
+            (turn, line[1..].parse().unwrap())
+        }).collect()
+}
+
+enum Turn {
+    Left,
+    Right
 }
